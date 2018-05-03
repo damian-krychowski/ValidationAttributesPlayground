837a43a9a26c0f5f6c532912dd732ca1e7cddc01
Damian Krychowski <damian.krychowski@gmail.com>
2018-05-03 10:40:22 +0200

    Tests of Required attribute and Range attribute for TimeSpans

---
+++ Source/ValidationAttributesPlayground/ValidationAttributesPlayground/RequiredAttributeTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ValidationAttributesPlayground
+{
+    public class RequiredAttributeTests
+    {
+        private const string ErrorMessage = "Bar is required";
+
+        [Test]
+        public void when_bar_is_null_validation_fails()
+        {
+            //given
+            var foo = new Foo
+            {
+                Bar = null
+            };
+
+            //when
+            Action validate = () => Validator.ValidateObject(foo, new ValidationContext(foo));
+
+            //then
+            validate
+                .Should().Throw<ValidationException>()
+                .Which.Message
+                .Should().Be(ErrorMessage);
+        }
+
+        [Test]
+        public void when_bar_is_not_null_foo_is_valid()
+        {
+            //given
+            var foo = new Foo
+            {
+                Bar = new object()
+            };
+
+            //when
+            Action validate = () => Validator.ValidateObject(foo, new ValidationContext(foo));
+
+            //then
+            validate.Should().NotThrow<ValidationException>();
+        }
+
+        private class Foo
+        {
+            [Required(ErrorMessage = ErrorMessage)]
+            public object Bar { get; set; }
+        }
+    }
+}
---
+++ Source/ValidationAttributesPlayground/ValidationAttributesPlayground/TimeSpanRangeAttributeTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ValidationAttributesPlayground
+{
+    public class TimeSpanRangeAttributeTests
+    {
+        [Test]
+        public void when_timespan_is_less_then_range_minimum_validation_fails()
+        {
+            //given
+            var sut = new TimeSpanRangedWithStrings
+            {
+                TimeSpan = TimeSpan.Zero
+            };
+
+            //then
+            Validate(sut).Should().Throw<ValidationException>();
+        }
+
+        [Test]
+        public void when_timespan_is_greater_then_range_maximum_validation_fails()
+        {
+            //given
+            var sut = new TimeSpanRangedWithStrings
+            {
+                TimeSpan = TimeSpan.FromHours(13)
+            };
+            
+            //then
+            Validate(sut).Should().Throw<ValidationException>();
+        }
+
+        [Test]
+        public void when_timespan_is_within_the_range_validation_passes()
+        {
+            //given
+            var sut = new TimeSpanRangedWithStrings
+            {
+                TimeSpan = TimeSpan.FromHours(11)
+            };
+            
+            //then
+            Validate(sut).Should().NotThrow<ValidationException>();
+        }
+
+        private class TimeSpanRangedWithStrings
+        {
+            [System.ComponentModel.DataAnnotations.Range(typeof(TimeSpan), "01:00:00", "12:00:00")]
+            public TimeSpan TimeSpan { get; set; }
+        }
+
+
+        [Test]
+        public void when_range_attribute_is_defined_as_integers_they_are_not_treated_as_milliseconds()
+        {
+            //given
+            var sut = new TimeSpanRangedWithInts
+            {
+                TimeSpan = TimeSpan.FromMilliseconds(150)
+            };
+            
+            //then
+            Validate(sut).Should().Throw<ValidationException>();
+        }
+
+        private class TimeSpanRangedWithInts
+        {
+            [System.ComponentModel.DataAnnotations.Range(100,200)]
+            public TimeSpan TimeSpan { get; set; }
+        }
+
+        private static Action Validate(object sut)
+        {
+            return () => Validator.ValidateObject(sut, new ValidationContext(sut), true);
+        }
+    }
+}
